a34afab534c585b9a2b913ef85e094233b79ddde
younesben <mohamedbenkhemis@yahoo.fr>
2014-05-21 02:40:17 +0200

    Asked tidy sets generated, script provided

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -1,56 +1,29 @@
 subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
 subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
 subject_merge<-rbind(subject_test,subject_train)
+names(subject_merge)<-"Subject_id"
 
 X_test<-read.table("UCI HAR Dataset/test/X_test.txt")
 X_train<-read.table("UCI HAR Dataset/train/X_train.txt")
 X_merge<-rbind(X_test,X_train)
 
-Y_test<-read.table("UCI HAR Dataset/test/Y_test.txt")
-Y_train<-read.table("UCI HAR Dataset/train/Y_train.txt")
-Y_merge<-rbind(Y_test,Y_train)
-
-body_acc_x_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
-body_acc_x_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-body_acc_x_merge<-rbind(body_acc_x_test,body_acc_x_train)
-
-body_acc_y_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt")
-body_acc_y_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-body_acc_y_merge<-rbind(body_acc_y_test,body_acc_y_train)
-
-body_acc_z_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt")
-body_acc_z_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-body_acc_z_merge<-rbind(body_acc_z_test,body_acc_z_train)
-
-body_gyro_x_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt")
-body_gyro_x_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-body_gyro_x_merge<-rbind(body_gyro_x_test,body_gyro_x_train)
-
-body_gyro_y_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt")
-body_gyro_y_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-body_gyro_y_merge<-rbind(body_gyro_y_test,body_gyro_y_train)
-
-body_gyro_z_test<-read.table("UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt")
-body_gyro_z_train<-read.table("UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-body_gyro_z_merge<-rbind(body_gyro_z_test,body_gyro_z_train)
-
-total_acc_x_test<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt")
-total_acc_x_train<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-total_acc_x_merge<-rbind(total_acc_x_test,total_acc_x_train)
-
-total_acc_y_test<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
-total_acc_y_train<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-total_acc_y_merge<-rbind(total_acc_y_test,total_acc_y_train)
-
-total_acc_z_test<-read.table("UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
-total_acc_z_train<-read.table("UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
-total_acc_z_merge<-rbind(total_acc_z_test,total_acc_z_train)
-
-
-
 measurement_labels<-read.table("UCI HAR Dataset/features.txt")
 colnames(X_merge)<-measurement_labels[,2]
 X_meansAndStds<-X_merge[,grep("mean|std",colnames(X_merge))]
 
+Y_test<-read.table("UCI HAR Dataset/test/Y_test.txt")
+Y_train<-read.table("UCI HAR Dataset/train/Y_train.txt")
+Y_merge<-rbind(Y_test,Y_train)
+names(Y_merge)<-"Activity"
 
-replace <- function(a){for (i in 1:6) {if (a==i) {a<-activity_labels[i]}}} a }
+
+activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
+Y_merge[,1]<-factor(Y_merge[,1])
+levels(Y_merge[,1])<-activity_labels[,2]
+
+completeTable<-cbind(subject_merge,cbind(Y_merge,X_meansAndStds))
+write.table(completeTable,"tidySet1.txt",row.names=F)
+
+Xmelt<-melt(completeTable,id=c("Activity","Subject_id"),measure.vars=names(completeTable[,3:81]))
+meansBySubjByAct<-dcast(Xmelt,Subject_id+Activity~variable,mean)
+write.table(meansBySubjByAct,"tidySet2.txt",row.names=F)
